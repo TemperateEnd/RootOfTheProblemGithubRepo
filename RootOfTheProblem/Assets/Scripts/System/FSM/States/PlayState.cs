dd50eb08d38cc04820e5f340fc7fb29f5ee8553a
Reece Tarrant <rtarrant70998@gmail.com>
2023-01-31 15:48:58 +0000

    Fully coded PlayState script file. For now, no further work will be needed for the FSM. So I can consider this done.

--- RootOfTheProblem/Assets/Scripts/System/FSM/States/PlayState.cs
+++ RootOfTheProblem/Assets/Scripts/System/FSM/States/PlayState.cs
@@ -3,3 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Assets.Scripts.FiniteStateMachine.Interfaces;
+
+namespace Assets.Scripts.FiniteStateMachine.States {
+    public class PlayState: IBaseState {
+        private StateManager stateManager;
+        private Scene scene;
+        public PlayState(StateManager stateManagerRef) {
+            stateManager = stateManagerRef;
+            scene = SceneManager.GetActiveScene();
+            if(scene.name != "PlayState"){
+                SceneManager.LoadScene("PlayState");
+            }
+        }
+    }
+}
