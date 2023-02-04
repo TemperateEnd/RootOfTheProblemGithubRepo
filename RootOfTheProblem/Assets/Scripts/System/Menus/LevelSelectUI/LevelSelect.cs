f0f0ecb4cfa62075ca0a5d0959be11f1c19489e8
Reece Tarrant <rtarrant70998@gmail.com>
2023-02-04 12:24:59 +0000

    Updated Level Select by adding code to move player to PlayState when selecting level

--- RootOfTheProblem/Assets/Scripts/System/Menus/LevelSelectUI/LevelSelect.cs
+++ RootOfTheProblem/Assets/Scripts/System/Menus/LevelSelectUI/LevelSelect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts.System.FSM.States;
 
 public class LevelSelect : MonoBehaviour {
     private void OnEnable() {
@@ -9,6 +10,7 @@ public class LevelSelect : MonoBehaviour {
 
     public void SelectLevel(Level chosenLevel) {
         EventManager.TriggerEvent("DisableLevelSelection");
+        StateManager.InstanceRef.SwitchState(new PlayState(StateManager.InstanceRef));
         LevelManager.SetSelectedLevel(chosenLevel);
         EventManager.TriggerEvent("GenerateLevel");
     }
